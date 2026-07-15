a8e4cad828ed48975ea145939ae99a9a9de50282
Sven Göthel <sgothel@jausoft.com>
2026-07-15 20:07:56 +0200

    scripts/twister.sh: Twister uni-platforms at once, smp one-by-one

--- scripts/twister.sh
+++ scripts/twister.sh
@@ -5,46 +5,26 @@ rootdir=`dirname $sdir`
 
 source ${sdir}/setenv-run.sh
 
-#
-# Manually resolving twister to silently drop platforms
-# when passing multiple platforms.
-# See https://github.com/zephyrproject-rtos/zephyr/issues/113359
-#
-twister_for_all() {
-	testsuite_root=$1
-	shift 1
-	for platform in "$@" ; do
-		platform2=$(echo "${platform}" | sed 's/\//_/g')
-		odir="twister-out.${platform2}"
-		if [ -e "${odir}" ] ; then
-			echo "Removing ${odir}"
-			rm -rf "${odir}"
-		fi
-		echo "west twister -O "${odir}" -p ${platform} -T ${testsuite_root}"
-		west twister -O "${odir}" -p ${platform} -T ${testsuite_root}
-	done
-}
-
 # smp
-ok_platforms="-p qemu_cortex_a53/qemu_cortex_a53/smp"
-ok_platforms="${ok_platforms} -p qemu_riscv64/qemu_virt_riscv64/smp"
-ok_platforms="${ok_platforms} -p qemu_x86_64/atom"
+ok_smp_platforms="-p qemu_cortex_a53/qemu_cortex_a53/smp"
+ok_smp_platforms="${ok_smp_platforms} -p qemu_riscv64/qemu_virt_riscv64/smp"
+ok_smp_platforms="${ok_smp_platforms} -p qemu_x86_64/atom"
 # uni
-ok_platforms="${ok_platforms} -p mps2/an385"
-ok_platforms="${ok_platforms} -p qemu_cortex_a53"
-ok_platforms="${ok_platforms} -p qemu_riscv64/qemu_virt_riscv64"
-ok_platforms="${ok_platforms} -p qemu_riscv32/qemu_virt_riscv32"
-ok_platforms="${ok_platforms} -p qemu_x86/atom"
+ok_uni_platforms="${ok_uni_platforms} -p mps2/an385"
+ok_uni_platforms="${ok_uni_platforms} -p qemu_cortex_a53"
+ok_uni_platforms="${ok_uni_platforms} -p qemu_riscv64/qemu_virt_riscv64"
+ok_uni_platforms="${ok_uni_platforms} -p qemu_riscv32/qemu_virt_riscv32"
+ok_uni_platforms="${ok_uni_platforms} -p qemu_x86/atom"
 
-ok_platforms2=$(echo ${ok_platforms} | sed 's/-p //g')
+ok_platforms="${ok_uni_platforms} ${ok_smp_platforms}"
 
 # SMP
 #platform="qemu_cortex_a53/qemu_cortex_a53/smp"
-platform="qemu_riscv64/qemu_virt_riscv64/smp"
+#platform="qemu_riscv64/qemu_virt_riscv64/smp"
 #platform="qemu_riscv32/qemu_virt_riscv32/smp"
-#platform="qemu_x86_64/atom"
+platform="qemu_x86_64/atom"
 
-smp_platforms="-p qemu_x86_64/atom -p qemu_cortex_a53/qemu_cortex_a53/smp -p qemu_riscv64/qemu_virt_riscv64/smp -p qemu_riscv32/qemu_virt_riscv32/smp"
+all_smp_platforms="-p qemu_x86_64/atom -p qemu_cortex_a53/qemu_cortex_a53/smp -p qemu_riscv64/qemu_virt_riscv64/smp -p qemu_riscv32/qemu_virt_riscv32/smp"
 
 # Uni
 #platform="mps2/an385"
@@ -55,22 +35,59 @@ smp_platforms="-p qemu_x86_64/atom -p qemu_cortex_a53/qemu_cortex_a53/smp -p qem
 #platform="qemu_riscv32e/qemu_virt_riscv32e"
 #platform="qemu_x86/atom"
 
-uni_platforms="-p qemu_x86/atom -p mps2/an385 -p qemu_riscv32e/qemu_virt_riscv32e"
-all_platforms="${uni_platforms} ${smp_platforms}"
+all_uni_platforms="-p qemu_x86/atom -p mps2/an385 -p qemu_riscv32e/qemu_virt_riscv32e"
+all_platforms="${all_uni_platforms} ${all_smp_platforms}"
 few_platforms="-p qemu_x86/atom -p mps2/an385 -p qemu_riscv32e/qemu_virt_riscv32e -p qemu_x86_64/atom -p qemu_riscv64/qemu_virt_riscv64/smp"
+
+twister_at_once() {
+	tag=$1
+	shift 1
+	testsuite_root=$1
+	shift 1
+	platforms="$*"
+	shift 1
+	odir="twister-out.${tag}"
+	if [ -e "${odir}" ] ; then
+		echo "Removing ${odir}"
+		rm -rf "${odir}"
+	fi
+	echo "west twister -O "${odir}" ${platforms} -T ${testsuite_root} -v"
+	west twister -O "${odir}" ${platforms} -T ${testsuite_root} -v
+}
+
+twister_for_each() {
+	tag=$1
+	shift 1
+	testsuite_root=$1
+	shift 1
+	platforms=$(echo "$*" | sed 's/-p //g')
+	for platform in ${platforms} ; do
+		platform2=$(echo "${platform}" | sed 's/\//_/g')
+		odir="twister-out.${tag}.${platform2}"
+		if [ -e "${odir}" ] ; then
+			echo "Removing ${odir}"
+			rm -rf "${odir}"
+		fi
+		echo "west twister -O "${odir}" -p ${platform} -T ${testsuite_root} -v"
+		west twister -O "${odir}" -p ${platform} -T ${testsuite_root} -v
+	done
+}
+
 
 do_run() {
 	which qemu
 
-	twister_for_all tests ${ok_platforms2}
-	# west twister ${ok_platforms} -T tests
-	# west twister -p ${platform} -T tests
+	twister_at_once ok_uni tests "${ok_uni_platforms}"
+	#twister_for_each smp tests "${ok_smp_platforms}"
+	#twister_at_once ok_smp tests "${ok_smp_platforms}"
+	#twister_at_once ok_all tests "${ok_platforms}"
+
+	#west twister -p ${platform} -T tests
 
 	# west twister -p ${platform} -T tests/pthread
 	# west twister -p ${platform} -T tests/cpp/
 
-	#twister_for_all tests/cpp/concurrency ${ok_platforms2}
-	#west twister ${ok_platforms} -T tests/cpp/concurrency
+	#west twister ${ok_platforms} -T tests/cpp/concurrency -v
 	#west twister -p ${platform} -T tests/cpp/concurrency
 
 	#west twister -p ${platform} -T tests/cpp/basic
@@ -88,5 +105,4 @@ do_run() {
 }
 
 do_run 2>&1 | tee twister.log
-grep "failed:" twister.log
 
